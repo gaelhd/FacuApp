a78af39558abf8e2ecb3d91add208fc23ca411df
gaelhd <87879748+gaelhd@users.noreply.github.com>
2023-03-22 12:53:35 -0600

    Add files via upload

--- JS.js
+++ JS.js
@@ -1,33 +1,16 @@
+/* JS index */
 
-/**/
-function IrRegistrar(){
-window.location.replace("Registro.html");
-
+function PagRegistrar(){
+    window.location.replace("Registro.html");
 }
 
+function PagIngresar(){
+    window.location.reload("Registro.html")    
+}
+
+/* JS Registro */
 
 function Registrar(){
-/* Captura de datos del login a localStorage */
-    var Correo = document.getElementById('correoReg').value;
-    var Contra = document.getElementById('contraReg').value;
-    var Nombre = document.getElementById('nombreReg').value;
-
-/* Guardar los datos en localStorage */
-
-    localStorage.setItem("Correo",Correo);
-    localStorage.setItem("Contra",Contra);
-    localStorage.setItem("Nombre",Nombre)
-
-/* Obtención de datos almacenados */
-    let correo = localStorage.getItem("Correo");
-    let contra = localStorage.getItem("Contra");
-    let nombre = localStorage.getItem("Nombre");
-
-/* CONDICION PARA AVISAR SI SE REGISTRO CORRECTAMENTE*/
-if(correo !== "" || contra !== "" || nombre !== "" ){
-    alert('Se agrego correctamente tu usuario, presione INGRESAR');
-    window.location.href="Registro.html";
-}else{
-    alert('Complete los datos antes de REGISTRAR')
+    
 }
-    }
+
